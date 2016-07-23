fc5af01e26b8f82d08a9ce66b3280e6faced0c58
ptallrights <ptallrights@163.com>
2016-07-23 16:56:08 +0800

    pygame

---
+++ SmallRoutine/Pygame.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding:utf-8
+# __author__ = 'ptallrights'
+
+"""
+@version: Python 2.7.11
+@author: ptallrights
+@license: Apache Licence 
+@file: Pygame.py.py
+@time: 2016/7/22 19:47
+"""
+
+import pygame
+import sys,random
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+screen.fill([255,255,255])
+#pygame.draw.circle(screen,[255,0,0],[320,240],30,0)
+#my_list = [250,150,300,200]
+#pygame.draw.rect(screen,[255,0,0],my_list,0)
+#my_rect = pygame.Rect(250,150,300,200)
+#pygame.draw.rect(screen,[255,255,0],my_rect,2)
+for i in range(100):
+    width = random.randint(0,250)
+    height = random.randint(0,100)
+    top = random.randint(0,400)
+    left = random.randint(0,500)
+    pygame.draw.rect(screen,[0,0,0],[left,top,width,height],1)
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
---
+++ SmallRoutine/getIP.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#coding=utf-8
+#__auther__ = 'Administrator'
+
+import requests
+import csv
+
+def getIP(file):
+    iplist = []
+    fi = open(file,'r')
+    for ip in fi:
+        ip = ip.strip()
+        iplist.append(ip)
+    return iplist
+
+def get_geolocation(ip):
+    r = requests.get('https://freegeoip.net/json/' + ip)
+    info = [str(r.json()['country_name']),str(r.json()['city'])]
+    return {'ip':ip,'country_name':info[0],'city_name':info[1]}
+
+if __name__ == '__main__':
+    iplist = getIP('E:/workspace/PycharmProjects/ipfile')
+    f = open('outputinfo.csv','a+')
+    fieldnames = ['ip','country_name','city_name']
+    dict_writer = csv.DictWriter(f,fieldnames=fieldnames)
+    dict_writer.writerow(dict(zip(fieldnames,fieldnames)))
+    for ip in iplist:
+        data = get_geolocation(ip)
+        dict_writer.writerow(data)
