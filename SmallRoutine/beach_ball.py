d73c7dd9c44ffaaa29c944c3285dec81e5e7fc65
ptallrights <ptallrights@163.com>
2016-07-23 21:27:24 +0800

    pygame-ball

---
+++ SmallRoutine/beach_ball.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding:utf-8
+# __author__ = 'ptallrights'
+
+"""
+@version: Python 2.7.11
+@author: ptallrights
+@license: Apache Licence 
+@file: beach_ball.py.py
+@time: 2016/7/23 20:43
+"""
+
+import pygame,sys
+class Myballclass(pygame.sprite.Sprite):
+   def __init__(self,image_file,location):
+       pygame.sprite.Sprite.__init__(self)
+       self.image=pygame.image.load(image_file)
+       self.rect = self.image.get_rect()
+       self.rect.left,self.rect.top=location
+size=width,height=640,480
+screen=pygame.display.set_mode(size)
+screen.fill([255,255,255])
+pygame.display.flip()
+img_file='beach_ball.png'
+balls=[]
+for row in range(0,3):
+   for column in range(0,3):
+       location = [column*180+10,row*180+10]
+       ball = Myballclass(img_file,location)
+       balls.append(ball)
+for ball in balls:
+   screen.blit(ball.image,ball.rect)
+pygame.display.flip()
+while True:
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           sys.exit()
